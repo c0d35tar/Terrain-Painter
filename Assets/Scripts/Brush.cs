46d9a7fdfa8cc50e98b3e4bc6d3b4cf15b6a9553
C0D35TAR <cody.joel.tate@gmail.com>
2018-09-10 19:40:57 +1000

    First Commit

---
+++ Assets/Scripts/Brush.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brush : MonoBehaviour
+{
+    public float scale = 0.01f;
+    public GameObject brush;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+    public void AdjustScale(float newScale)
+    {
+        
+        scale = newScale;
+        brush.transform.localScale = Vector3.one * scale;
+    }
+}
---
+++ Assets/Scripts/Paintable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class Paintable : MonoBehaviour
+{
+    public GameObject Brush;
+    public RenderTexture renderTexture;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	    if (Input.GetMouseButton(0))
+	    {
+	        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+	        RaycastHit hit;
+	        if (Physics.Raycast(ray, out hit))
+	        {
+	            var paint = Instantiate(Brush, hit.point + Vector3.up * 0.1f, Quaternion.identity, transform);
+	        }
+	    }
+
+	}
+
+    public void Save()
+    {
+        StartCoroutine(CoSave());
+    }
+
+    private IEnumerator CoSave()
+    {
+        yield return new WaitForEndOfFrame();
+        Debug.Log(Application.dataPath + "/savedImage.png");
+
+        RenderTexture.active = renderTexture;
+
+        var texture2D = new Texture2D(renderTexture.width, renderTexture.height);
+        texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        texture2D.Apply();
+
+        var data = texture2D.EncodeToPNG();
+
+        File.WriteAllBytes(Application.dataPath + "/savedImage.png", data);
+    }
+}
